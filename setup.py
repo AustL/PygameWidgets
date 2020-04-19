a8e40317b20bc70c33641ca305ac7506ca42832d
Austin Lin <21chydra@gmail.com>
2020-04-19 10:00:58 +1000

    Created package setup files

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    longDescription = f.read()
+
+setuptools.setup(
+    name='pygame-widgets',
+    version='0.0.3',
+    author='AustL',
+    author_email='21chydra@gmail.com',
+    description='Widgets for use with Pygame',
+    long_description=longDescription,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AustL/PygameWidgets',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6',
+    license='MIT'
+)
