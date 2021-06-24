99d80a8495a1e879b9e93e414cf1f6eda8e7d3ad
Austin Lin <21chydra@gmail.com>
2021-06-24 11:53:58 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='pygame-widgets',
-    version='0.5.0',
+    version='0.5.1',
     author='AustL',
     author_email='21chydra@gmail.com',
     description='Widgets for use with Pygame',
