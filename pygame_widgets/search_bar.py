6f0637a96663b1bdd4d0985d460dfe4acac2bb0b
lionel42 <43442120+lionel42@users.noreply.github.com>
2021-07-22 23:55:03 +0200

    implemented basic bones

--- pygame_widgets/search_bar.py
+++ pygame_widgets/search_bar.py
@@ -1,13 +1,14 @@
 import pygame
 
 from pygame_widgets.widget import WidgetBase
+from pygame_widgets.textbox import TextBox
 
 
 class SearchBar(WidgetBase):
-    def __init__(self, win, x, y, width, height, **kwargs):
-        """A customisable search bar for Pygame.
+    def __init__(self, win, x, y, width, height, choices, **kwargs):
+        """Initialize a customisable search bar for Pygame.
 
-        It is possible to write in the bar.
+        The bar can be written on, and displays under it the results.
 
         :param win: Surface on which to draw
         :type win: pygame.Surface
@@ -19,5 +20,43 @@ class SearchBar(WidgetBase):
         :type width: int
         :param height: Height of button
         :type height: int
+        :param choices: Possible search values
+        :type choices: list(str)
         :param kwargs: Optional parameters
         """
+        super().__init__(win, x, y, width, height)
+        self._dropped = False
+        self.__chosen = None
+
+        self.choices = choices
+
+        self.text_bar = TextBox(win, x, y, width, height, **kwargs)
+
+    def listen(self, events):
+        """Wait for input.
+
+        :param events: Use pygame.event.get()
+        :type events: list of pygame.event.Event
+        """
+        if not self._hidden:
+            self.text_bar.listen(events)
+
+    def draw(self):
+        """Draw the widget."""
+        if not self._hidden:
+            self.text_bar.draw()
+            if self.dropped:
+                print('Must show possbilities.')
+                print('Should also check wheter to update')
+
+
+    @property
+    def dropped(self):
+        return self._dropped
+
+    @dropped.setter
+    def dropped(self, new_dropped):
+        if isinstance(new_dropped, bool):
+            self._dropped = new_dropped
+        else:
+            raise TypeError('Wrong type for \'dropped\' property, boolean is expected')
